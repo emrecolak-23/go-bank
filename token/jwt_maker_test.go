e52b720f0ddb35dc91ba08734aa5c57bf7810fac
emrecolak <emre.c@playablefactory.com>
2025-10-28 23:33:23 +0300

    create jwt and paseto token

---
+++ token/jwt_maker_test.go
@@ -0,0 +1,54 @@
+package token
+
+import (
+	"testing"
+	"time"
+
+	"github.com/emrecolak-23/go-bank/utils"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/stretchr/testify/require"
+)
+
+func TestJWTMaker(t *testing.T) {
+	maker, err := NewJWTMaker(utils.RandomString(32))
+	require.NoError(t, err)
+
+	username := utils.RandomOwner()
+	duration := time.Minute
+
+	issuedAt := time.Now()
+	expiredAt := issuedAt.Add(duration)
+
+	token, err := maker.CreateToken(username, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.NoError(t, err)
+	require.NotEmpty(t, payload)
+
+	// Düzeltmeler:
+	require.NotZero(t, payload.ID)
+	require.Equal(t, username, payload.Username)
+	require.WithinDuration(t, issuedAt, payload.IssuedAt, time.Second)
+	require.WithinDuration(t, expiredAt, payload.ExpiresAt, time.Second)
+}
+
+func TestInvalidJWTTokenAlgNode(t *testing.T) {
+
+	payload, err := NewPayload(utils.RandomOwner(), time.Minute)
+	require.NoError(t, err)
+
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodNone, payload)
+	token, err := jwtToken.SignedString(jwt.UnsafeAllowNoneSignatureType)
+	require.NoError(t, err)
+
+	maker, err := NewJWTMaker(utils.RandomString(32))
+	require.NoError(t, err)
+
+	payload, err = maker.VerifyToken(token)
+	require.Error(t, err)
+	require.ErrorIs(t, err, ErrInvalidToken)
+	require.Nil(t, payload)
+
+}
---
+++ token/paseto_maker_test.go
@@ -0,0 +1,35 @@
+package token
+
+import (
+	"testing"
+	"time"
+
+	"github.com/emrecolak-23/go-bank/utils"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPasetoMaker(t *testing.T) {
+
+	maker, err := NewPasetoMaker(utils.RandomString(32))
+	require.NoError(t, err)
+
+	username := utils.RandomOwner()
+	duration := time.Minute
+
+	issuedAt := time.Now()
+	expiredAt := issuedAt.Add(duration)
+
+	token, err := maker.CreateToken(username, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.NoError(t, err)
+	require.NotEmpty(t, payload)
+
+	require.NotZero(t, payload.ID)
+	require.Equal(t, username, payload.Username)
+	require.WithinDuration(t, issuedAt, payload.IssuedAt, time.Second)
+	require.WithinDuration(t, expiredAt, payload.ExpiresAt, time.Second)
+
+}
