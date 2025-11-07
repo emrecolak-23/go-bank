e84dc78aea1782a05463a5746e2514d9361485a6
emrecolak <emre.c@playablefactory.com>
2025-11-08 00:58:22 +0300

    validate gPrc parameters and human/machine firendly reposne

--- gapi/rpc_create_user.pb.go
+++ gapi/rpc_create_user.pb.go
@@ -6,12 +6,21 @@ import (
 	db "github.com/emrecolak-23/go-bank/db/sqlc"
 	"github.com/emrecolak-23/go-bank/pb"
 	"github.com/emrecolak-23/go-bank/utils"
+	"github.com/emrecolak-23/go-bank/val"
 	"github.com/lib/pq"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
 func (server *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest) (*pb.CreateUserResponse, error) {
+
+	violations := validateCreateUserRequest(req)
+
+	if violations != nil {
+		return nil, invalidArgumentError(violations)
+	}
+
 	hashedPassword, err := utils.HashPassword(req.GetPassword())
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, "failed to hash password: %v", err)
@@ -40,4 +49,24 @@ func (server *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest)
 	}
 
 	return rsp, nil
+}
+
+func validateCreateUserRequest(req *pb.CreateUserRequest) (violations []*errdetails.BadRequest_FieldViolation) {
+	if err := val.ValidateUsername(req.GetFullName()); err != nil {
+		violations = append(violations, fieldViolation("username", err))
+	}
+
+	if err := val.ValidatePassword(req.GetPassword()); err != nil {
+		violations = append(violations, fieldViolation("password", err))
+	}
+
+	if err := val.ValidateUsersFullname(req.GetFullName()); err != nil {
+		violations = append(violations, fieldViolation("full_name", err))
+	}
+
+	if err := val.ValidateEmailAddress(req.GetEmail()); err != nil {
+		violations = append(violations, fieldViolation("email_address", err))
+	}
+
+	return violations
 }
---
+++ val/validator.go
@@ -0,0 +1,64 @@
+package val
+
+import (
+	"fmt"
+	"net/mail"
+	"regexp"
+)
+
+var (
+	isValidUsername     = regexp.MustCompile(`^[a-z0-9_]+$`).MatchString
+	isValidUserFullname = regexp.MustCompile(`^[a-zA-Z\\s]+$`).MatchString
+)
+
+func ValidateString(value string, minLength int, maxLength int) error {
+	n := len(value)
+
+	if n < minLength || n > maxLength {
+		return fmt.Errorf("must contain %d-%d characters", minLength, maxLength)
+	}
+
+	return nil
+}
+
+func ValidateUsername(value string) error {
+	if err := ValidateString(value, 3, 100); err != nil {
+		return err
+	}
+
+	if !isValidUsername(value) {
+		return fmt.Errorf("must contain only lowercase letters, digits and underscore")
+	}
+
+	return nil
+}
+
+func ValidatePassword(value string) error {
+	return ValidateString(value, 6, 100)
+}
+
+func ValidateEmailAddress(value string) error {
+	if err := ValidateString(value, 3, 200); err != nil {
+		return err
+	}
+
+	_, err := mail.ParseAddress(value)
+
+	if err != nil {
+		return fmt.Errorf("is not a valid email address")
+	}
+
+	return nil
+}
+
+func ValidateUsersFullname(value string) error {
+	if err := ValidateString(value, 3, 100); err != nil {
+		return err
+	}
+
+	if !isValidUserFullname(value) {
+		return fmt.Errorf("must contain only letters or spaces")
+	}
+
+	return nil
+}
