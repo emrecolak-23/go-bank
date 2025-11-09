0c7a85773fae9101072458a600ec50a1108b3c6c
emrecolak <emre.c@playablefactory.com>
2025-11-09 22:02:42 +0300

    send email via gmail

---
+++ mail/sender_test.go
@@ -0,0 +1,39 @@
+package mail
+
+import (
+	"testing"
+
+	"github.com/emrecolak-23/go-bank/utils"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSendEmailWithGmail(t *testing.T) {
+	config, err := utils.LoadConfig("..")
+	require.NoError(t, err)
+	require.NotEmpty(t, config)
+
+	sender := NewGmailSender(
+		config.EmailSenderName,
+		config.EmailSenderAddress,
+		config.EmailSenderPassword,
+	)
+
+	subject := "A test mail"
+	content := `
+		<h1>Hello World!</h1>
+		<p>This is a test message</p>
+	`
+	to := []string{"colakkemre@gmail.com"}
+	attachFiles := []string{"../Readme.md"}
+
+	err = sender.SendEmail(
+		subject,
+		content,
+		to,
+		nil,
+		nil,
+		attachFiles,
+	)
+
+	require.NoError(t, err)
+}
